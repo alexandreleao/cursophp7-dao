82b15d3144eb4bd655b2c4d8962c58f6d582151d
Alexandre <erdleosalgueiro@gmail.com>
2020-02-15 21:05:09 -0300

    metodos de lista

--- class/Sql.php
+++ class/Sql.php
@@ -28,7 +28,7 @@ public function query($rawQuery, $params = array()){
 
 	$stmt = $this->conn->prepare($rawQuery);
 
-   $this->setParams($stmt, $params);
+   $this->setParams($stmt,$params);
 	$stmt->execute();
  
     return $stmt;
--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
 
 require_once("config.php");
+//carrega um usuário.
+//$root = new  Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$root = new  Usuario();
+//Carrega uma lista de usuários.
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-$root->loadbyId(3);
+//Carrega uma lista de usuários buscando pelo login.
 
-echo $root;
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha.
+
+$usuario = new Usuario();
+$usuario->login("jose","1234567890");
+
+//echo $usuario;
+
+
 
 ?>
